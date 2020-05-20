e722e596fbb2c31be491c68760e6e7dbc91127b0
gunjan mistry <gunjan.mistry@somaiya.edu>
2020-05-21 01:37:55 +0530

    make it better

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(require('./routes/user'))
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
-    const path = require(path)
+    const path = require('path')
     app.get('*', (res,req) => {
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
