13a32b590946d61f72a600d85c2b3134a3cd7460
gunjan mistry <gunjan.mistry@somaiya.edu>
2020-05-21 01:03:09 +0530

    make it better

---
+++ client/build/precache-manifest.0d23853729c72b187c35d05af9b691f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b90575a7a5ebb64d729e86da4958b2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57ec501280c202a95042",
+    "url": "/static/css/main.da25eb33.chunk.css"
+  },
+  {
+    "revision": "9931e316b068c110e92e",
+    "url": "/static/js/2.c5843fc0.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.c5843fc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57ec501280c202a95042",
+    "url": "/static/js/main.00b708da.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
